ffa5156cbc85b862d59d8d59849bf83cdaa15e44
agsoto02 <alejandrogarciasoto02@gmail.com>
2020-08-16 10:34:39 -0400

    base mainframe

--- Application/src/gui/MainFrame.java
+++ Application/src/gui/MainFrame.java
@@ -3,5 +3,17 @@ package gui;
 import javax.swing.JFrame;
 
 public class MainFrame extends JFrame {
+	
+	private static final int WIDTH = 500, HEIGHT = 800;
+	
+	public MainFrame() {
+		super("Application 1.0");
+		
+		setSize(WIDTH, HEIGHT);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
 
 }
